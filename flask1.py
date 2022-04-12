cc753411af95a8a1ef710f9b03009bd9fc0dad6f
CaioFeitosa <caio@lojaspintos.com.br>
2022-04-12 12:18:51 -0300

    Fazendo testes 1

--- flask1.py
+++ flask1.py
@@ -4,9 +4,14 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
-db = MySQL(app)
+# db = MySQL(app)
 
-from views import *
+# from views import *
+
+
+@app.route('/')
+def index():
+    return 'Hello World'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
